371a6b05bfdc2b2e37992ed969f017d495f9062f
adamcodes716 <adamcodes716@gmail.com>
2020-04-17 08:49:57 -0400

    finished

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'saa-app',
+  encapsulation: ViewEncapsulation.None,
   template: `
     <header>
       <h1>Styling Angular Applications</h1>
